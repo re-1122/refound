e13b32991faaf50923f1d80e61c3747b7e47bd87
Komichi <k0michi@koyomi.co>
2024-01-15 10:18:14 +0900

    Annotate ServerProxy with @Mod.EventBusSubscriber for consistency

--- src/main/java/com/koyomiji/refound/proxy/ServerProxy.java
+++ src/main/java/com/koyomiji/refound/proxy/ServerProxy.java
@@ -2,10 +2,15 @@ package com.koyomiji.refound.proxy;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import com.koyomiji.refound.ReFound;
 import net.minecraft.client.Minecraft;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.relauncher.Side;
 
+@Mod.EventBusSubscriber(modid = ReFound.MODID, value = Side.SERVER)
 public class ServerProxy extends CommonProxy {
   @Override
   public Path getCacheDir() {
